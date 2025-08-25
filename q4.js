299479cbe43f574889d4f0793765f1c9790147a4
WihansiChandrasiri <wihansichandrasiri@gmail.com>
2025-08-25 14:24:58 +0530

    Create q4.js

---
+++ q4.js
@@ -0,0 +1,16 @@
+//q4
+//scale a matrix
+function scaleMatrix(matrix, scalar) {
+  return matrix.map(row => row.map(value => value * scalar));
+}
+
+
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+let scaled = scaleMatrix(matrix, 3);
+
+console.log(scaled);
